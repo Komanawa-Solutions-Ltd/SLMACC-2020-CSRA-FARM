de63d73056447ef3c054f40b08b6c4a1b7384309
matt_dickie <dumont.environmental@gmail.com>
2021-02-26 14:25:25 +1300

    setup baseline for another test

--- Storylines/storyline_runs/base_scen.py
+++ Storylines/storyline_runs/base_scen.py
@@ -9,18 +9,19 @@ import time
 import ksl_env
 import os
 from Climate_Shocks.climate_shocks_env import storyline_dir
-from Pasture_Growth_Modelling.full_model_implementation import run_pasture_growth
+from Pasture_Growth_Modelling.full_model_implementation import run_pasture_growth, default_pasture_growth_dir
 from BS_work.SWG.SWG_wrapper import *
 
 if __name__ == '__main__':
-    #todo set up with new datsets run on dickie
-    run_basgra = True
+    #todo set up with new datsets run on dickie , and REVIEW
+    # todo may need to re-run with new irrigation restrictions...
+    run_basgra = False  # to stop accidental re-run
 
     if run_basgra:
     # run basgra
         print('running BASGRA')
-        run_pasture_growth(storyline_key='0-base',
-                           outdir=os.path.join(ksl_env.slmmac_dir_unbacked, 'pasture_growth_sims'),
-                           nsims='all', padock_rest=True,
-                           save_daily=True, description='initial baseline run note that this was run before fixing '
-                                                        'the swg matching errors e.g. realisation cleaning')
+        run_pasture_growth(storyline_path=os.path.join(storyline_dir, '0-baseline.csv'),
+                           outdir=os.path.join(default_pasture_growth_dir, 'baseline_sim'),
+                           nsims=10000, padock_rest=True,
+                           save_daily=True, description='initial baseline run after the realisation cleaning, '
+                                                        'have not finalized irrigation restirctions')
