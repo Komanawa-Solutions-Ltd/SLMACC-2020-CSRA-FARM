2749e5721e2635920f8ad1c2d6c4eeb5e5b5a490
matt_dickie <dumont.environmental@gmail.com>
2021-03-15 16:50:21 +1300

    get lauras_v2.py running

--- Storylines/storyline_runs/lauras_v2.py
+++ Storylines/storyline_runs/lauras_v2.py
@@ -4,12 +4,13 @@
  """
 
 import pandas as pd
+import numpy as np
 import ksl_env
 import os
 import itertools
 import glob
 from copy import deepcopy
-from Storylines.storyline_building_support import base_events, map_irrigation, default_storyline_time
+from Storylines.storyline_building_support import base_events, map_storyline_rest, default_storyline_time
 from Storylines.check_storyline import ensure_no_impossible_events
 from Climate_Shocks import climate_shocks_env
 from Pasture_Growth_Modelling.full_pgr_model_mp import default_pasture_growth_dir, run_full_model_mp, pgm_log_dir
@@ -31,15 +32,31 @@ for d in [story_dir, base_pg_outdir, outputs_dir]:
 
 
 def make_storylines():
-    test = pd.read_excel(r"M:\Shared drives\Z2003_SLMACC\storylines\blank_storylineWS120321.xlsx", skiprows=2,
-                         index_col=[0, 1, 2, 3], header=None) #todo update path
+    test = pd.read_excel(os.path.join(ksl_env.slmmac_dir, r"storylines\blank_storylineWS120321.xlsx"), skiprows=2,
+                         index_col=[0, 1, 2, 3], header=None)
     header_1 = test.iloc[0].ffill().values
     header_2 = test.iloc[1].values
     storylines = pd.DataFrame(index=pd.MultiIndex.from_arrays([default_storyline_time.year,
                                                                default_storyline_time.month], names=['year', 'month']),
-                              columns=pd.MultiIndex.from_arrays([header_1, header_2]), data=test.iloc[2:].values)
+                              columns=pd.MultiIndex.from_arrays([header_1, header_2]), data=test.iloc[3:].values)
+    good_stories = list(set(storylines.columns.get_level_values(0)) - {'Storyline Name'})
 
-    raise NotImplementedError()
+    for s in sorted(good_stories):
+        print(s)
+        sl = storylines.loc[:, s].reset_index()
+        assert sl.shape == (36, 5), f'shape problem with {s}'
+        sl.loc[:, 'rest'] = pd.to_numeric(sl.loc[:, 'rest'])
+        sl.loc[pd.isnull(sl.rest), 'rest'] = 50
+        sl.loc[:, 'rest'] *= 1 / 100
+        sl.loc[pd.isnull(sl.precip_class), 'precip_class'] = 'A'
+        sl.loc[:, 'precip_class'] = sl.loc[:, 'precip_class'].str.replace('P', '').str.strip()
+        sl.loc[pd.isnull(sl.temp_class) & (
+            np.in1d(sl.month, [6, 7])), 'temp_class'] = 'C'  # todo defaults hard coded in...
+        sl.loc[pd.isnull(sl.temp_class), 'temp_class'] = 'A'
+        sl.loc[:, 'temp_class'] = sl.loc[:, 'temp_class'].str.replace('T', '').str.strip()
+        map_storyline_rest(sl)
+        s2 = s.replace('/','-').replace('(','').replace(')','').replace(',','')
+        sl.to_csv(os.path.join(story_dir, f'{s2}.csv'))
 
 
 def run_pasture_growth_mp():
@@ -48,7 +65,7 @@ def run_pasture_growth_mp():
     run_full_model_mp(
         storyline_path_mult=paths,
         outdir_mult=outdirs,
-        nsims_mult=10000,
+        nsims_mult=1000,
         log_path=os.path.join(pgm_log_dir, 'lauras'),
         description_mult='a first run of Lauras storylines',
         padock_rest_mult=False,
@@ -78,8 +95,8 @@ def export_and_plot_data():
 
 
 if __name__ == '__main__':
-    run = False
-    plot_export = False
+    run = True
+    plot_export = True
     export_csv = True
     make_storylines()
     if run:
