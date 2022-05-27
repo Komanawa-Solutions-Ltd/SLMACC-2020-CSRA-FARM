fd3de7d307bfea56fda2fbfb70e4a8de97ac9810
Matt_Dumont <hansonmcoombs@gmail.com>
2022-05-27 13:20:00 +1200

    add wanganui to repo so I can run it

--- ksl_env.py
+++ ksl_env.py
@@ -20,6 +20,7 @@ user = getuser()
 
 def mh_backed(path):
     host_nm_dict = {
+
         'dickie': {
             'dumon': r"M:\My Drive\MH_work_backed"},
         'Healey': {
@@ -39,14 +40,17 @@ def mh_backed(path):
 
 def shared_drives(path):
     host_nm_dict = {
+        'wanganui': {
+            'matt_dumont': '/home/matt_dumont/google_mount_point/'
+        },
         'dickie': {
-            'dumon': "M:/",
-            'matt_dumont': "M:/",
+            'dumon': "M:/Shared drives",
+            'matt_dumont': "M:/Shared drives",
         },
         'Healey': {
-            'Matt Hanson': "M:/"},
+            'Matt Hanson': "M:/Shared drives"},
         'DESKTOP-G2QSULJ': {
-            'Matt Hanson': "M:/"}
+            'Matt Hanson': "M:/Shared drives"}
     }
 
     if hostname not in host_nm_dict.keys():
@@ -55,11 +59,14 @@ def shared_drives(path):
     if user not in host.keys():
         raise ValueError('{} not in established users for host {}'.format(user, host))
 
-    return os.path.join(host[user], "Shared drives", path)
+    return os.path.join(host[user], path)
 
 
 def mh_unbacked(path):
     host_nm_dict = {
+        'wanganui': {
+            'matt_dumont': '/home/matt_dumont/unbacked'
+        },
         'dickie': {
             'dumon': "D:/mh_unbacked",
             'matt_dumont': "D:/mh_unbacked"
@@ -84,6 +91,9 @@ def mh_unbacked(path):
 
 def tempfiles(path):
     host_nm_dict = {
+        'wanganui': {
+            'matt_dumont': '/home/matt_dumont/temp_files'
+        },
         'dickie': {
             'dumon': r"D:\temp_files"},
         'Healey': {
@@ -108,6 +118,9 @@ def tempfiles(path):
 
 def add_basgra_nz_path():
     host_nm_dict = {
+        'wanganui': {
+            'matt_dumont': '/home/matt_dumont/PycharmProjects/BASGRA_NZ_PY'
+        },
         'dickie': {
             'matt_dumont': r'C:\Users\matt_dumont\PycharmProjects\BASGRA_NZ_PY',
             'dumon': r"C:\Users\dumon\python_projects\BASGRA_NZ_PY",
@@ -129,6 +142,10 @@ def add_basgra_nz_path():
 
 def get_vscn_dir():
     host_nm_dict = {
+        'wanganui': {
+            'matt_dumont': '/home/matt_dumont/temp_files'  # todo dummy
+        },
+
         'dickie': {
             'dumon': r"D:\SLMMAC_SWG/SLMACC-Subset_vcsn",
             'matt_dumont': r"D:\SLMMAC_SWG/SLMACC-Subset_vcsn"
