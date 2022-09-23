107ed28a42bceb9c2562440b819892bfadf8bea5
Evelyn_Charlesworth <charlesworth.evelyn@gmail.com>
2022-09-23 15:00:23 +1200

    adding in polynomials

--- Ecological_flows/evelyn_stats/ecological_scoring.py
+++ Ecological_flows/evelyn_stats/ecological_scoring.py
@@ -22,17 +22,65 @@ species_coeffs = {
     "longfin_eel": (-9.045618237519400E-09, 3.658952327544510E-06,
                     5.653574369241410E-04, 3.858556802202370E-02,
                     3.239955996233250E-01, 9.987638834796250E+01),
+    "shortfin_eel": (-5.964114493071940E-09, + 2.359764378654360E-06,
+                     - 3.693579872009160E-04, + 2.683927613703320E-02,
+                     - 3.681012446881110E-01,  + 8.593725263391190E+01),
+    "torrent_fish": (2.896163694304270E-08, 1.167620629575640E-05,
+                     + 1.801041895279500E-03, - 1.329402534268910E-01,
+                     + 5.277167341236740E+00, - 1.408366189647840E+01),
+    "common_bully": (3.679138046845140E-09, 1.938607130429040E-07,
+                     - 1.923502238925680E-04, + 2.961375443166340E-02,
+                     - 1.112066360882710E+00, + 7.329526111040610E+01),
+    "upland_bully": (-1.670386190380080E-08, + 7.480690123013630E-06,
+                     - 1.257177384401630E-03, + 9.648051249735090E-02,
+                     - 3.077836962111130E+00, + 8.675954558492810E+01),
+    "bluegill_bully": (-6.471586231748120E-09, + 1.973356622447410E-06,
+                       - 1.949914099179170E-04, + 5.570337619808730E-03,
+                       + 3.944431105242500E-01, + 3.459956435653860E+01),
+    "food_production": (2.130431975429750E-08, - 9.085807849474580E-06,
+                        + 1.464737145368640E-03, - 1.125512066047600E-01,
+                        + 4.823875351509410E+00, + 1.115625470423880E+01),
+    "brown_trout_adult": (4.716969949537670E-09, - 2.076496120868080E-06,
+                          + 3.361640291880770E-04, - 2.557607121249140E-02,
+                          + 1.060052581008110E+00, + 3.627596900757210E+0),
+    "chinook_salmon_junior": (6.430228856812380E-09, - 1.901413063448040E-06,
+                              + 1.779162094752800E-04, - 5.287064285669480E-03,
+                              + 6.690264788207550E-02, + 2.160739430906840E+01),
+    "diatoms": (7.415806641571640E-11, - 3.448627575182280E-08,
+                + 6.298888857172090E-06, - 5.672527158325650E-04,
+                + 2.595917911761800E-02, - 1.041530354852930E-01),
+    "long_filamentous": (-2.146620894005660E-10, + 8.915219136657130E-08,
+                         - 1.409667339556760E-05, + 1.057153790947640E-03,
+                         - 3.874332961128240E-02, + 8.884973169426100E-01),
+    "short_filamentous": (1.411793860210670E-10, - 5.468836816918290E-08,
+                          + 7.736645471349440E-06, - 4.767919019192250E-04,
+                          + 1.082051321324740E-02, + 3.578139911667070E-01),
+    "black_fronted_tern": (1.860374649942380E-06, - 6.206129788530580E-04,
+                           + 8.139025742665820E-02, - 5.222181017852630E+00,
+                           + 1.629785813832450E+02, - 1.908796384066770E+03),
+    "wrybill_plover": (1.992991145099990E-05, - 6.562761816460400E-03,
+                       + 8.524578863075030E-01, - 5.444835223306980E+01,
+                       + 1.702284174702220E+03, - 2.058208449588460E+04)
+
+
 
 }
 species_limits = {
-    "longfin_eel": (18, 180),
+    "longfin_eel": (18, 180), "shortfin_eel": (18, 180), "torrent_fish": (18, 180),
+    "common_bully": (18, 180), "upland_bully": (18, 180), "bluegill_bully": (18, 180),
+    "food_production": (18, 180), "brown_trout_adult": (18, 180), "chinook_salmon_junior": (18, 180),
+    "diatoms": (18, 180), "long_filamentous": (18, 180), "short_filamentous": (18, 180),
+"black_fronted_tern": (35, 85), "wrybill_plover": (35, 85)
 }
 
 species_max_wua = {
-    "longfin_eel": 172,  # todo check
+    "longfin_eel": 172, "shortfin_eel": 132, "torrent_fish": 120, "common_bully": 87,
+    "upland_bully": 71, "bluegill_bully": 75, "food_production": 150, "brown_trout_adult": 30,
+"chinook_salmon_junior": 27, "diatoms": 0.42, "long_filamentous": 0.42, "short_filamentous": 0.43,
+"black_fronted_tern": 66, "wrybill_plover": 203
 }
 
-
+#todo change so that the max wua is for malf value
 def get_dataset():
     base_path = kslcore.KslEnv.shared_gdrive.joinpath(
         'Z2003_SLMACC/eco_modelling/stats_info/66401_Naturalised_flow.csv')
@@ -69,89 +117,6 @@ def flow_to_wua(alf, species):
         wua, wua_per = None, None
     return wua, wua_per
 
-
-def flow_to_wua_old(dataframe, species_name):
-    """A function (that is not the best) but calculates
-    the WUA for each species for each ALF and appends it to the correct DF.
-    It also calculates the WUA % based on the max WUA"""
-    species_names = ["longfin_eel"]
-    assert species_name in species_names, f'species name: {species_name} not in accepted list:{species_names}'
-    WUA_list = []
-    WUA_percen_list = []
-    for index, row in dataframe.iterrows():
-        x = row['ALF']
-        WUA = "Sorry, flow is out of range"
-        WUA_percen = "Sorry, flow is out of range"
-        if species_name == "longfin_eel":
-            if x > 18 and x < 180:
-                WUA = -9.045618237519400E-09 * x ** 5 + 3.658952327544510E-06 * x ** 4 - 5.653574369241410E-04 * x ** 3 + 3.858556802202370E-02 * x ** 2 - 3.239955996233250E-01 * x + 9.987638834796250E+01
-                WUA_percen = WUA / 176 * 100
-
-        elif species_name == "shortfin_eel":
-            if x > 18 and x < 180:
-                WUA = -5.964114493071940E-09 * x ** 5 + 2.359764378654360E-06 * x ** 4 - 3.693579872009160E-04 * x ** 3 + 2.683927613703320E-02 * x ** 2 - 3.681012446881110E-01 * x + 8.593725263391190E+01
-                WUA_percen = WUA / 132 * 100
-
-        elif species_name == 'torrent_fish':
-            if x > 18 and x < 180:
-                WUA = 2.896163694304270E-08 * x ** 5 - 1.167620629575640E-05 * x ** 4 + 1.801041895279500E-03 * x ** 3 - 1.329402534268910E-01 * x ** 2 + 5.277167341236740E+00 * x - 1.408366189647840E+01
-                WUA_percen = WUA / 120 * 100
-
-        elif species_name == 'common_bully':
-            if x > 18 and x < 180:
-                WUA = 3.679138046845140E-09 * x ** 5 - 1.938607130429040E-07 * x ** 4 - 1.923502238925680E-04 * x ** 3 + 2.961375443166340E-02 * x ** 2 - 1.112066360882710E+00 * x + 7.329526111040610E+01
-                WUA_percen = WUA / 87 * 100
-
-        elif species_name == 'upland_bully':
-            if x > 18 and x < 180:
-                WUA = -1.670386190380080E-08 * x ** 5 + 7.480690123013630E-06 * x ** 4 - 1.257177384401630E-03 * x ** 3 + 9.648051249735090E-02 * x ** 2 - 3.077836962111130E+00 * x + 8.675954558492810E+01
-                WUA_percen = WUA / 71 * 100
-
-        elif species_name == 'bluegill_bully':
-            if x > 18 and x < 180:
-                WUA = -6.471586231748120E-09 * x ** 5 + 1.973356622447410E-06 * x ** 4 - 1.949914099179170E-04 * x ** 3 + 5.570337619808730E-03 * x ** 2 + 3.944431105242500E-01 * x + 3.459956435653860E+01
-                WUA_percen = WUA / 75 * 100
-        elif species_name == 'food_production':
-            if x > 18 and x < 180:
-                WUA = 2.130431975429750E-08 * x ** 5 - 9.085807849474580E-06 * x ** 4 + 1.464737145368640E-03 * x ** 3 - 1.125512066047600E-01 * x ** 2 + 4.823875351509410E+00 * x + 1.115625470423880E+01
-                WUA_percen = WUA / 150 * 100
-        elif species_name == 'brown_trout_adult':
-            if x > 18 and x < 180:
-                WUA = 4.716969949537670E-09 * x ** 5 - 2.076496120868080E-06 * x ** 4 + 3.361640291880770E-04 * x ** 3 - 2.557607121249140E-02 * x ** 2 + 1.060052581008110E+00 * x + 3.627596900757210E+00
-                WUA_percen = WUA / 30 * 100
-        elif species_name == 'chinook_salmon_juv':
-            if x > 18 and x < 180:
-                WUA = 6.430228856812380E-09 * x ** 5 - 1.901413063448040E-06 * x ** 4 + 1.779162094752800E-04 * x ** 3 - 5.287064285669480E-03 * x ** 2 + 6.690264788207550E-02 * x + 2.160739430906840E+01
-                WUA_percen = WUA / 27 * 100
-        elif species_name == 'diatoms':
-            if x > 18 and x < 180:
-                WUA = 7.415806641571640E-11 * x ** 5 - 3.448627575182280E-08 * x ** 4 + 6.298888857172090E-06 * x ** 3 - 5.672527158325650E-04 * x ** 2 + 2.595917911761800E-02 * x - 1.041530354852930E-01
-                WUA_percen = WUA / 0.42 * 100
-        elif species_name == 'long_filamentous':
-            if x > 18 and x < 180:
-                WUA = -2.146620894005660E-10 * x ** 5 + 8.915219136657130E-08 * x ** 4 - 1.409667339556760E-05 * x ** 3 + 1.057153790947640E-03 * x ** 2 - 3.874332961128240E-02 * x + 8.884973169426100E-01
-                WUA_percen = WUA / 0.42 * 100
-        elif species_name == 'short_filamentous':
-            if x > 18 and x < 180:
-                WUA = 1.411793860210670E-10 * x ** 5 - 5.468836816918290E-08 * x ** 4 + 7.736645471349440E-06 * x ** 3 - 4.767919019192250E-04 * x ** 2 + 1.082051321324740E-02 * x + 3.578139911667070E-01
-                WUA_percen = WUA / 0.43 * 100
-        elif species_name == 'black_fronted_tern':
-            if x > 35 and x < 85:
-                WUA = 1.860374649942380E-06 * x ** 5 - 6.206129788530580E-04 * x ** 4 + 8.139025742665820E-02 * x ** 3 - 5.222181017852630E+00 * x ** 2 + 1.629785813832450E+02 * x - 1.908796384066770E+03
-                WUA_percen = WUA / 66 * 100
-        elif species_name == 'wrybill_plover':
-            if x > 35 and x < 85:
-                WUA = 1.992991145099990E-05 * x ** 5 - 6.562761816460400E-03 * x ** 4 + 8.524578863075030E-01 * x ** 3 - 5.444835223306980E+01 * x ** 2 + 1.702284174702220E+03 * x - 2.058208449588460E+04
-                WUA_percen = WUA / 203 * 100
-
-            WUA_list.append(WUA)
-            WUA_percen_list.append(WUA_percen)
-        else:
-            raise ValueError('should not get here')
-
-    dataframe[f'WUA score for {species_name}'] = WUA_list
-    dataframe[f"% of max WUA for {species_name}"] = WUA_percen_list
-    return dataframe
 
 
 def read_and_stats(outpath, start_water_year, end_water_year, flow_limits=None):
