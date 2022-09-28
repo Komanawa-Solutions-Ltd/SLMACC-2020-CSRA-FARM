ca343d0bb8106cad55010f3d8f2990a8d15df3d2
Evelyn_Charlesworth <charlesworth.evelyn@gmail.com>
2022-09-29 10:35:58 +1300

    getting daily avg

---
+++ Ecological_flows/evelyn_stats/temp_data.py
@@ -0,0 +1,15 @@
+"""
+created Evelyn_Charlesworth 
+on: 29/09/2022
+"""
+
+import kslcore
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+from itertools import groupby
+from kslcore import KslEnv
+
--- Ecological_flows/evelyn_stats/water_temp_monthly.py
+++ Ecological_flows/evelyn_stats/water_temp_monthly.py
@@ -10,11 +10,17 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import datetime
 from itertools import groupby
+import kslcore
+from kslcore import KslEnv
 
-data = pd.read_csv("V:\\Shared drives\\Z2003_SLMACC\\eco_modelling\\temp_data\\Waiau_Uwha_tidied.csv")
-print(data.head())
+base_path = kslcore.KslEnv.shared_gdrive.joinpath("Z2003_SLMACC/eco_modelling/temp_data/Waiau_Uwha_tidied.csv")
+data = pd.read_csv(base_path)
 # converting into PeriodIndex and then getting the mean
 
-mean_df = data.groupby(pd.PeriodIndex(data["Date & Time"], freq="M"))['Water Temp (degC)'].mean()
+monthly_mean_df = data.groupby(pd.PeriodIndex(data["Date & Time"], freq="M"))['Water Temp (degC)'].mean()
 
-mean_df.to_csv("V:\\Shared drives\\Z2003_SLMACC\\eco_modelling\\temp_data\\Waiau_monthly_mean.csv")
+#monthly_mean_df.to_csv("V:\\Shared drives\\Z2003_SLMACC\\eco_modelling\\temp_data\\Waiau_monthly_mean.csv")
+
+daily_mean_df = data.groupby(pd.PeriodIndex(data["Date & Time"], freq="D"))['Water Temp (degC)'].mean()
+save_path = kslcore.KslEnv.shared_gdrive.joinpath("Z2003_SLMACC/eco_modelling/temp_data/Waiau_daily_mean.csv")
+daily_mean_df.to_csv(save_path)
