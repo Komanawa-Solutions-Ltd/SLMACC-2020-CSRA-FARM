505d5faffec781c5e1925351ef77110c6bc24e50
Evelyn_Charlesworth <charlesworth.evelyn@gmail.com>
2022-10-25 11:07:21 +1300

    checking code

--- Ecological_flows/evelyn_stats/max_water_temp.py
+++ Ecological_flows/evelyn_stats/max_water_temp.py
@@ -21,7 +21,7 @@ from sklearn.linear_model import LinearRegression
 # reading in the daily air temp
 
 water_base_path = kslcore.KslEnv.shared_gdrive.joinpath("Z2003_SLMACC/eco_modelling/temp_data/Waiau_daily_max.csv")
-daily_max_df = pd.read_csv(water_base_path)
+daily_max_water_df = pd.read_csv(water_base_path)
 base_path = kslcore.KslEnv.shared_gdrive.joinpath("Z2003_SLMACC/eco_modelling/temp_data/waiau_temp/daily_temperature_Cheviot Ews_data.csv")
 mean_air_temp_df = pd.read_csv(base_path)
 mean_air_temp_df = mean_air_temp_df.rename(columns={'time': 'date'})
@@ -29,14 +29,14 @@ mean_air_temp_df['date'] = pd.to_datetime(mean_air_temp_df['date'], format='%Y/%
 
 #
 
-#daily_max_df['Date & Time'] = pd.to_datetime(daily_max_df['Date & Time'], format='%Y/%m/%d')
-#daily_max_df = daily_max_df.rename(columns={'Date & Time' : 'date', 'Water Temp (degC)': 'daily_max_water_temp'})
-#merged_df = daily_max_df.merge(mean_air_temp_df)
-#merged_df.to_csv(kslcore.KslEnv.shared_gdrive.joinpath("Z2003_SLMACC/eco_modelling/temp_data/waiau_temp/air_water_max_temp_data.csv"))
-#
-#regression_df = merged_df.dropna()
-#x = regression_df.loc[:,'daily_mean_air_temp'].values.reshape(-1, 1)
-#y = regression_df.loc[:,'daily_max_water_temp'].values.reshape(-1, 1)
-#temp_regr = LinearRegression()
-#temp_regr.fit(x, y)
-#print(temp_regr.score(x,y))
+daily_max_water_df['Date & Time'] = pd.to_datetime(daily_max_water_df['Date & Time'], format='%Y/%m/%d')
+daily_max_water_df = daily_max_water_df.rename(columns={'Date & Time' : 'date', 'Water Temp (degC)': 'daily_max_water_temp'})
+merged_df = daily_max_water_df.merge(mean_air_temp_df)
+
+regression_df = merged_df.dropna()
+x = regression_df.loc[:,'daily_mean_air_temp'].values.reshape(-1, 1)
+y = regression_df.loc[:,'daily_max_water_temp'].values.reshape(-1, 1)
+temp_regr = LinearRegression()
+temp_regr.fit(x, y)
+print(temp_regr.score(x,y))
+pass
