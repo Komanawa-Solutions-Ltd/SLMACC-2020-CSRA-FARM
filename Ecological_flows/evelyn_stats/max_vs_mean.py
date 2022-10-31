15e7aee9ddfb6af1bf2e99f091e6656dd1ca6114
Evelyn_Charlesworth <charlesworth.evelyn@gmail.com>
2022-11-01 10:54:16 +1300

    adding in comments to make clearer

--- Ecological_flows/evelyn_stats/max_vs_mean.py
+++ Ecological_flows/evelyn_stats/max_vs_mean.py
@@ -44,20 +44,27 @@ month_list = {'jan': 1, 'feb': 2, 'mar':3, 'apr':4, 'may':5, 'jun':6, 'jul':7, '
 predicted_waimak_df = pd.DataFrame()
 
 for month, num in month_list.items():
+    # subset of the waiau data that we want to use to fit
     waiau_monthly_df = daily_mean_and_max_temp[daily_mean_and_max_temp['month'] == num]
     waiau_monthly_df = waiau_monthly_df.dropna()
+    # subset of the waikmakariri data that we want to use to predict
     waimak_monthly_temp = waimak_daily_mean_water[waimak_daily_mean_water['month'] == num]
     waimak_monthly_temp = waimak_monthly_temp.dropna()
+    # reshaping for regression
     x = waiau_monthly_df.loc[:, 'daily_mean_water_temp'].values.reshape(-1, 1)
     y = waiau_monthly_df.loc[:, 'daily_max_water_temp'].values.reshape(-1, 1)
     z = waimak_monthly_temp.loc[:, 'mean_daily_water_temp'].values.reshape(-1, 1)
     max_mean_temp_regr = LinearRegression()
+    # fitting the regression between the waiau daily mean water temp and daily max water temp
     max_mean_temp_regr.fit(x, y)
+    # using the relationship to predict the waimakariri max daily water temp from the mean daily water temp
     waimak_monthly_temp.loc[:, 'predicted_daily_max_water_temp'] = max_mean_temp_regr.predict(z)
     frames = [predicted_waimak_df, waimak_monthly_temp]
+    # concatenating the dataframes together so that at the end we have all the months
+    # each month is predicted by its individual relationship
     predicted_waimak_df = pd.concat(frames)
     print(max_mean_temp_regr.score(x,y))
-    plt.scatter(x, y)
+    #plt.scatter(x, y)
     #plt.show()
 
 predicted_waimak_df.to_csv(kslcore.KslEnv.shared_gdrive.joinpath("Z2003_SLMACC/eco_modelling/temp_data/waimak_daily_max_temp_predicted.csv"))
